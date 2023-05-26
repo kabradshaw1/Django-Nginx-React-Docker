62a9d92f09f95445f0dd82773f36cb22c596e613
kabradshaw1 <itllbok82@gmail.com>
2023-05-26 11:15:03 -0400

    working on containers

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [appState, setAppState] = useState({
+		loading: false,
+		posts: null,
+	});
+
+
+	useEffect(() => {
+		setAppState({ loading: true, posts: null });
+		const apiUrl = `http://127.0.0.1/api/`;
+		fetch(apiUrl)
+			.then((data) => data.json())
+			.then((posts) => {
+				setAppState({ loading: false, posts: posts });
+        console.log(appState)
+			});
+	}, [setAppState]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>Check Logs</h1>
   );
 }
 
